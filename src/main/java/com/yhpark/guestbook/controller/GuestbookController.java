c0d76f68328001005fed1dd93b532c68b5cea330
yunhwan <pyh8618@gmail.com>
2021-03-07 00:56:08 +0900

    list & register Contoller - Thymeleaf 연동 완료

--- src/main/java/com/yhpark/guestbook/controller/GuestbookController.java
+++ src/main/java/com/yhpark/guestbook/controller/GuestbookController.java
@@ -1,5 +1,6 @@
 package com.yhpark.guestbook.controller;
 
+import com.yhpark.guestbook.dto.GuestbookDTO;
 import com.yhpark.guestbook.dto.PageRequestDTO;
 import com.yhpark.guestbook.service.GuestbookService;
 import lombok.RequiredArgsConstructor;
@@ -8,7 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/guestbook")
@@ -31,6 +34,23 @@ public class GuestbookController {
 
         // requestDTO를 넘겨주고 resultDTO를 받아올 수 있다. (조회)
         model.addAttribute("result", service.getList(pageRequestDTO));
+    }
+
+    @GetMapping("/register")
+    public void register() {
+        log.info("register get....");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes) {
+        log.info("dto..." + dto);
+
+        Long gno = service.register(dto);
+
+        // 단 한 번만 데이터를 전달하는 용
+        redirectAttributes.addFlashAttribute("msg", gno);
+
+        return "redirect:/guestbook/list";
     }
 }
  
